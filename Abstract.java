2c100aba47b5a07d39b7d7a48c27192f25039bab
Ashurai84 <raia40094@gmail.com>
2025-03-20 10:19:15 +0530

    Added Abstract class and Interface example - Circle

---
+++ Abstract.java
@@ -0,0 +1,93 @@
+// abstract  class  Animal{
+//     abstract void makesound();
+
+//     void sleep(){
+//         System.out.println("Sleeping.....");
+//     }}
+
+//     class dog extends Animal{
+//         @Override
+//         void makesound(){
+//             System.out.println("dogs barking........");
+//         }
+//     }
+//    public class Abstract {
+   
+//     public static void main(String[] args) {
+//         dog d = new dog();
+//         d.makesound();
+//         d.sleep();
+//     }
+//    }
+
+
+
+
+
+
+// abstract  class  vehicle {
+//     abstract void engine();
+
+//     void v8(){
+//         System.out.println("this sis very power full engine");
+//     }}
+
+ 
+//         class  Suzuki  extends vehicle{
+//     @Override
+//     void engine(){
+//         System.out.println("this is labo and no comparasion from it ");
+//     }
+// }
+
+// public class Abstract {
+
+//     public static void main(String[] args) {
+//         Suzuki s = new Suzuki();
+//         s.engine();
+//         s.v8();
+//     }
+// }
+
+
+// Abstract Class Shape
+abstract class Shape {
+    abstract void area();  
+}
+
+// Interface Drawable
+interface Drawable {
+    void draw();  
+}
+
+// Circle Class extends Shape and implements Drawable
+class Circle extends Shape implements Drawable {
+    double radius;
+
+    // Constructor
+    Circle(double r) {
+        radius = r;
+    }
+
+    // Implement area() method of Shape
+    void area() {
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+
+    // Implement draw() method of Drawable
+    public void draw() {
+        System.out.println("Drawing Circle...");
+    }
+}
+
+// Main Class
+public class Abstract {
+    public static void main(String[] args) {
+        Circle c = new Circle(5);  
+        c.draw();   
+        c.area();     
+    }
+}
+
+
